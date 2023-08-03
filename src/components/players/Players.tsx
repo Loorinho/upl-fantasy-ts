b844b28f1c2557c80aa505760ac2d07c723d64ce
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-03 18:50:58 +0300

    Update Players.tsx
    removed unnecessary import on players page

--- src/components/players/Players.tsx
+++ src/components/players/Players.tsx
@@ -1,18 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
 import { Player, fetchPlayers } from "../../zustand/api/api";
-
 const Players = () => {
   const { data: players, isLoading } = useQuery({
     queryKey: ["lecturers"],
     queryFn: fetchPlayers,
   });
-  console.log("Players: ", players)
   return (
     <div>
       <table className="table-auto w-90 ">
         <thead>
-          <tr className="px-2 py-2">
+          <tr className="px-2 py-2 bg-gray-400">
             <th>Name</th>
             <th>Shirt No.</th>
             <th>Position</th>
