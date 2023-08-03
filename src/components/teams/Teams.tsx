ec38139906bed9d39df499e366b287c6261db041
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-03 19:52:13 +0300

    Update Teams.tsx
    Fetched players using react query

--- src/components/teams/Teams.tsx
+++ src/components/teams/Teams.tsx
@@ -1,4 +1,10 @@
 const Teams = () => {
+    const { data: players, isLoading } = useQuery({
+    queryKey: ["lecturers"],
+    queryFn: fetchPlayers,
+  });
+
+  console.log("Plyers: ", players)
   return (
     <div>Teams list</div>
   )
