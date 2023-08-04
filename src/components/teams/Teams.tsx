0e6dded75f11a83d58c1b54ee384a0f8e1709c75
Jacobson <romeojacobson@gmail.com>
2023-08-04 04:10:07 +0300

    Added managers page

--- src/components/teams/Teams.tsx
+++ src/components/teams/Teams.tsx
@@ -1,12 +1,35 @@
-const Teams = () => {
-    const { data: players, isLoading } = useQuery({
-    queryKey: ["lecturers"],
-    queryFn: fetchPlayers,
-  });
+import { useQuery } from "@tanstack/react-query";
+import { fetchTeams } from "../../zustand/api/api";
 
-  console.log("Plyers: ", players)
+const Teams = () => {
+    const { data: teams, isLoading } = useQuery({
+      queryKey: ["teams"],
+      queryFn: fetchTeams,
+    });
+
+  console.log("Teams: ", teams)
   return (
-    <div>Teams list</div>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>City</th>
+                    <th>Country</th>
+                </tr>
+            </thead>
+            <tbody>
+                {teams?.map((team: any) => (
+                    <tr key={team.id}>
+                        <td>{team.name}</td>
+                        <td>{team.city}</td>
+                        <td>{team.country}</td>
+                    </tr>
+                )
+                )}
+            </tbody>
+        </table>
+    </div>
   )
 }
 
