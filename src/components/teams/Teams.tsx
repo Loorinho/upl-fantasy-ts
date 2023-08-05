82e493749f53d751fd511bb5dd4e441ed6e4bd40
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-05 07:56:12 +0300

    Update Teams.tsx
    Added table styles to teams table

--- src/components/teams/Teams.tsx
+++ src/components/teams/Teams.tsx
@@ -8,21 +8,34 @@ const Teams = () => {
   });
 
   return (
-    <div>
-      <table>
-        <thead>
+  <div style={{ width: '500px' }}>
+      <table className="w-full border-2 border-b-gray-400">
+        <thead className="bg-gray-50 bottom-2 border-gray-200">
           <tr>
-            <th>Name</th>
-            <th>City</th>
-            <th>Country</th>
+            <th className=" w-10 p-2 text-sm font-semibold tracking-wide text-left">
+              No.
+            </th>
+            <th className=" w-50 p-2 text-sm font-semibold tracking-wide text-left">
+              Name
+            </th>
+            <th className=" w-10 p-2 text-sm font-semibold tracking-wide text-left">
+              City
+            </th>
+            <th className=" w-30 p-2 text-sm font-semibold tracking-wide text-left">
+              Country
+            </th>
           </tr>
         </thead>
-        <tbody>
-          {teams?.map((team: any) => (
+        <tbody className="divide-y divide-gray-400">
+          {teams?.map((team: any, index) => (
             <tr key={team.id}>
-              <td>{team.name}</td>
-              <td>{team.city}</td>
-              <td>{team.country}</td>
+              <td className="p-2 text-sm text-gray-700 ">{index + 1}</td>
+
+              <td className="p-2 text-sm text-gray-700 ">
+                {team.name}
+              </td>
+              <td className="p-2 text-sm text-gray-700 ">{team.city}</td>
+              <td className="p-2 text-sm text-gray-700 ">{team.country}</td>
             </tr>
           ))}
         </tbody>
