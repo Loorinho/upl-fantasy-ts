ff23cf38d40659d931635073e95d3fb2befe30bf
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-05 07:23:49 +0300

    Update Players.tsx
    Added table styles to players page

--- src/components/players/Players.tsx
+++ src/components/players/Players.tsx
@@ -6,17 +6,52 @@ const Players = () => {
     queryFn: fetchPlayers,
   });
   return (
-    <div>
-      <table className="table-auto player_table border-collapse border border-slate-500 ">
-        <thead>
-          <tr className="px-2 py-2 bg-gray-400">
-            <th>Name</th>
-            <th>Shirt No.</th>
-            <th>Position</th>
-            <th>Foot</th>
-            <th>Age</th>
+     <div style={{ width: '500px' }}>
+      <table className="w-full border-2 border-b-gray-400">
+        <thead className="bg-gray-50 bottom-2 border-gray-200">
+          <tr>
+            <th className=" w-10 p-2 text-sm font-semibold tracking-wide text-left">
+              No.
+            </th>
+            <th className=" w-50 p-2 text-sm font-semibold tracking-wide text-left">
+              Name
+            </th>
+            <th className=" w-10 p-2 text-sm font-semibold tracking-wide text-left">
+              Shirt no.
+            </th>
+             <th className=" w-10 p-2 text-sm font-semibold tracking-wide text-left">
+              Age
+            </th>
+            <th className=" w-30 p-2 text-sm font-semibold tracking-wide text-left">
+              Position
+            </th>
+             <th className=" w-30 p-2 text-sm font-semibold tracking-wide text-left">
+              Foot
+            </th>
           </tr>
         </thead>
+        <tbody className="divide-y divide-gray-400">
+          {managers?.map((manager: any, index) => (
+            <tr key={manager.id}>
+              <td className="p-2 text-sm text-gray-700 ">{index + 1}</td>
+
+              <td className="p-2 text-sm text-gray-700 ">
+                {manager.first_name} {manager.last_name}
+              </td>
+              <td className="p-2 text-sm text-gray-700 ">{manager.age}</td>
+              <td className="p-2 text-sm text-gray-700 ">{manager.team}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+
+
+
+    
+    <div>
+      <table className="table-auto player_table border-collapse border border-slate-500 ">
+
         <tbody>
           {players?.map((player: Player) => (
             <tr key={player.id} className="text-center">
