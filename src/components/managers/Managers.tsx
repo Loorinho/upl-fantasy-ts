b129ea707219fb6f55dfd7ec808b9b6f10b4094e
Jacobson <romeojacobson@gmail.com>
2023-08-05 07:19:16 +0300

    Added tale styles to managers page

--- src/components/managers/Managers.tsx
+++ src/components/managers/Managers.tsx
@@ -7,24 +7,39 @@ const Managers = () => {
     queryFn: fetchManagers,
   });
 
+  if(isLoading){
+    return <p>Loading...</p>
+  }
+
   return (
-    <div>
-      <table>
-        <thead>
+    <div style={{ width: '500px' }}>
+      <table className="w-full border-2 border-b-gray-400">
+        <thead className="bg-gray-50 bottom-2 border-gray-200">
           <tr>
-            <th>Name</th>
-            <th>Age</th>
-            <th>Team</th>
+            <th className=" w-10 p-2 text-sm font-semibold tracking-wide text-left">
+              No.
+            </th>
+            <th className=" w-50 p-2 text-sm font-semibold tracking-wide text-left">
+              Name
+            </th>
+            <th className=" w-10 p-2 text-sm font-semibold tracking-wide text-left">
+              Age
+            </th>
+            <th className=" w-30 p-2 text-sm font-semibold tracking-wide text-left">
+              Team
+            </th>
           </tr>
         </thead>
-        <tbody>
-          {managers?.map((manager: any) => (
+        <tbody className="divide-y divide-gray-400">
+          {managers?.map((manager: any, index) => (
             <tr key={manager.id}>
-              <td>
+              <td className="p-2 text-sm text-gray-700 ">{index + 1}</td>
+
+              <td className="p-2 text-sm text-gray-700 ">
                 {manager.first_name} {manager.last_name}
               </td>
-              <td>{manager.age}</td>
-              <td>{manager.team}</td>
+              <td className="p-2 text-sm text-gray-700 ">{manager.age}</td>
+              <td className="p-2 text-sm text-gray-700 ">{manager.team}</td>
             </tr>
           ))}
         </tbody>
