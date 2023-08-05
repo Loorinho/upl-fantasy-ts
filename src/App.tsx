92526a57efe87249bad92bf4c845ed585923e805
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-05 07:46:19 +0300

    Update App.tsx
    Added player and team creation routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import Home from "./Home";
 import Players from "./components/players/Players";
+import CreatePlayer from "./components/players/CreatePlayer";
 import Teams from "./components/teams/Teams";
+import CreateTeam from "./components/teams/CreateTeam";
 import Managers from "./components/managers/Managers";
 
 function App() {
@@ -14,7 +16,9 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
        <Route path="/players" element={<Players />} />
+       <Route path="/players/create" element={<CreatePlayer />} />
        <Route path="/teams" element={<Teams />} />
+       <Route path="/teams/create" element={<CreateTeam />} />
       <Route path="/managers" element={<Managers />} />
     </Routes>
   );
