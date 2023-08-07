dbccbfb4f19bafca53eb9e2266a527e35ee9e5f6
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-07 21:20:25 +0300

    Update Sidebar.tsx
    Added sidebar links

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,5 +1,19 @@
+import {NavLink} from "react-router-dom"
+
 const Sidebar = () => {
-  return <div className="sidebar">Sidebar</div>;
+  return (
+  <div className="sidebar">
+    <div className="flex justify-center items-center flex-col">
+      <p>Loor Jacobson</p>
+      <p>Administrator</p>
+    </div>
+    <div className="nav_items">
+      <NavLink to="/players">Players</NavLink>
+      <NavLink to="/managers">Managers</NavLink>
+      <NavLink to="/teams">Teams</NavLink>
+    </div>
+  </div>
+ );
 };
 
 export default Sidebar;
