2a125ee1980a2e3e1685d98074ae5c516921ddb2
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-09 04:17:51 +0300

    Update CreateTeam.tsx
    Added player creation form fields

--- src/components/teams/CreateTeam.tsx
+++ src/components/teams/CreateTeam.tsx
@@ -1,6 +1,9 @@
-import { forwardRef } from 'react'
+import { forwardRef, useState } from 'react'
 type CreateTeamProps = {}
 const CreateTeam = forwardRef<HTMLDialogElement, CreateTeamProps>(({}, ref) => {
+
+  const [name, setName] = useState("")
+  const [city, setCity] = useState("")
   return <div>
    <form
         style={{ width: "500px" }}
@@ -11,6 +14,39 @@ const CreateTeam = forwardRef<HTMLDialogElement, CreateTeamProps>(({}, ref) => {
         onClick={() => ref?.current?.close()}
         >X</p>
         <p className="text-center py-4">Create a team</p>
+
+          <div className="mt-2">
+              <label className="text-gray-600 block text-sm">Team name</label>
+              <input
+                type="text"
+                value={name}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setName(e.target.value)
+                }
+                className="px-3 py-1.5 w-full rounded-md border border-blue-600 focus:ring-1 focus:ring-blue-600 outline-none"
+              />
+            </div>
+            <div className="mt-2">
+              <label className="text-gray-600 block text-sm">Last name</label>
+              <input
+                type="text"
+                value={city}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setCity(e.target.value)
+                }
+                className="px-3 py-1.5 w-full rounded-sm border border-blue-600 focus:ring-1 focus:ring-blue-600 outline-none"
+              />
+            </div>
+
+         <div className="flex justify-center items-center my-3">
+            <button
+              type="button"
+              className="bg-blue-600 rounded text-white px-3 py-2 w-full"
+             // onClick={() => handleSubmit()}
+            >
+              Create team
+            </button>
+          </div>
      </form>
   </div>;
 });
