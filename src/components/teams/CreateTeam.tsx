bde7671231de5dda18e327143fe3b2259ea37916
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-09 04:08:47 +0300

    Update CreateTeam.tsx
    Added team creation form

--- src/components/teams/CreateTeam.tsx
+++ src/components/teams/CreateTeam.tsx
@@ -1,5 +1,18 @@
-const CreateTeam = () => {
-  return <div>Team creation form</div>;
-};
+import { forwardRef } from 'react'
+type CreateTeamProps = {}
+const CreateTeam = forwardRef<HTMLDialogElement, CreateTeamProps>(({}, ref) => {
+  return <div>
+   <form
+        style={{ width: "500px" }}
+        className="rounded-md p-1 relative"
+        // onSubmit={(e: React.ChangeEvent<HTMLSelectElement>) => handleSubmit}
+      >
+        <p className="w-5 h-5 bg-red-600 text-white absolute right-1 cursor-pointer rounded-full text-center"
+        onClick={() => ref?.current?.close()}
+        >X</p>
+        <p className="text-center py-4">Create a team</p>
+     </form>
+  </div>;
+});
 
 export default CreateTeam;
