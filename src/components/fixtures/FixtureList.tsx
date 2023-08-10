9d1f8a814424dbaf6a9b3a0d9f84eadb7f6edf24
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-10 17:32:22 +0300

    Update FixtureList.tsx
    Added fetching of fixtures within the FixturesList page

--- src/components/fixtures/FixtureList.tsx
+++ src/components/fixtures/FixtureList.tsx
@@ -1,4 +1,17 @@
+import axios from 'axios';
 const FixturesList = () => {
+  const fetchFixtures = async () => {
+    const url = "http://127.0.0.1:8000/api/fixtures";
+  try{
+    const response = await axios.get(url)
+    console.log(response.data)
+  }catch(error){
+    console.error(error)
+  }
+  }
+  useEffect(() => {
+    fetchFixtures()
+  }, [])
   return (
     <div>Fixtures list</div>
   )
