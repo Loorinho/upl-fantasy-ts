9c181ada2b2e12941604fe61116c5b7ca6725e28
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-11 16:13:55 +0300

    Update Fixture.tsx
    Added fixture content in fixture component

--- src/components/fixtures/Fixture.tsx
+++ src/components/fixtures/Fixture.tsx
@@ -1,6 +1,17 @@
-const Fixture = () => {
+export type Fixture = {
+  id: number,
+  home_team: string,
+  away_team: string,
+  time: string
+}
+export type FixtureProps = {
+  fixture: Fixture
+}
+const Fixture = ({fixture}: FixtureProps ) => {
   return (
-    <div>Fixture</div>
+     <div key={fixture.id} className="text-center">
+        <p><span className="mr-3">Team {fixture.home_team}</span> <span className="mr-3">{fixture.time}</span> <span>Team {fixture.away_team}</span></p>       
+     </div> 
   )
 }
 
