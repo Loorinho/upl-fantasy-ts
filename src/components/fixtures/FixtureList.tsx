65b85eaa719ac8624ee7d04fc2b7c123f9e086a0
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-11 04:36:20 +0300

    Update FixtureList.tsx
    Looped through the fixtures

--- src/components/fixtures/FixtureList.tsx
+++ src/components/fixtures/FixtureList.tsx
@@ -1,9 +1,11 @@
 import axios from 'axios';
 const FixturesList = () => {
+  const [fixtures, setFixtures] = useState([])
   const fetchFixtures = async () => {
     const url = "http://127.0.0.1:8000/api/fixtures";
   try{
     const response = await axios.get(url)
+    setFixtures(response.data?.fixtures)
     console.log(response.data)
   }catch(error){
     console.error(error)
@@ -14,7 +16,15 @@ const FixturesList = () => {
     
   }, [])
   return (
-    <div>Fixtures list</div>
+    <div className="mt-5">
+      {
+        fixtures?.map((fixture: any) => (
+         <div key={fixture.id} className="text-center">
+             <p><span className="mr-3">{fixture.home_team}</span> <span className="mr-3">{fixture.time}</span> <span>{fixture.away_team}</span></p>       
+         </div> 
+        ))
+      }
+    </div>
   )
 }
 
