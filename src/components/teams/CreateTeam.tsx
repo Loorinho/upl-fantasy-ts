f0debaa16df2a0c95478877a9f7b0f8801b45fed
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-13 18:15:48 +0300

    Update CreateTeam.tsx
    Added a function to create a team

--- src/components/teams/CreateTeam.tsx
+++ src/components/teams/CreateTeam.tsx
@@ -1,14 +1,35 @@
 import { forwardRef, useState } from 'react'
+import axios from 'axios'
+
 type CreateTeamProps = {}
-const CreateTeam = forwardRef<HTMLDialogElement, CreateTeamProps>(({}, ref) => {
+const CreateTeam = forwardRef<HTMLDialogElement, CreateTeamProps>((CreateTeamProps, ref) => {
 
   const [name, setName] = useState("")
   const [city, setCity] = useState("")
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const data = {
+        name,
+        city
+    }
+    try{
+      const url = "http://localhost:8000/api/teams"
+      const response  = await axios.post(url,{ name,city}, {
+        headers: {
+          "Accept": "application/json"
+        }
+      })
+      console.log(response.data)  
+    }catch(error){
+      console.error(error)
+    }
+  }
   return <div className="px-4 py-2">
    <form
         style={{ width: "450px" }}
         className="p-1 relative"
-        // onSubmit={(e: React.ChangeEvent<HTMLSelectElement>) => handleSubmit}
+         onSubmit={(e) => handleSubmit(e)}
       >
         <p className="w-5 h-5 bg-red-600 text-white absolute right-1 cursor-pointer rounded-full text-center"
         onClick={() => ref?.current?.close()}
@@ -40,7 +61,7 @@ const CreateTeam = forwardRef<HTMLDialogElement, CreateTeamProps>(({}, ref) => {
 
          <div className="flex justify-center items-center my-3">
             <button
-              type="button"
+              type="submit"
               className="bg-blue-600 rounded text-white px-3 py-2 w-full"
              // onClick={() => handleSubmit()}
             >
