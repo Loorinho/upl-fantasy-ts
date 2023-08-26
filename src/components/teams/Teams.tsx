e97fd4ba402f1fbe1b37ecd89f742979475d8dfc
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-26 17:55:55 +0300

    Update Teams.tsx
    Fixed the dialog ref

--- src/components/teams/Teams.tsx
+++ src/components/teams/Teams.tsx
@@ -5,9 +5,16 @@ import CreateTeam from "./CreateTeam"
 const Teams = () => {
   const teamRef = useRef<HTMLDialogElement>(null);
   const teams = useUplStore((state) => state.teams);
+
+  const showModal = () => {
+    teamRef.current?.showModal();
+  }
+  const closeModal = () => {
+    teamRef.current?.close();
+  }
   return (
     <div style={{ width: "500px" }}>
-      <button className="bg-blue-700 text-white px-4 py-2 m-2 outline-none rounded" onClick={()=> teamRef.current?.showModal()}>
+      <button className="bg-blue-700 text-white px-4 py-2 m-2 outline-none rounded" onClick={showModal}>
       Create Team
       </button>
       <table className="w-full border-2 border-b-gray-400">
@@ -41,7 +48,7 @@ const Teams = () => {
       </table>
 
       <dialog className="team-dialog rounded-md" ref={teamRef}>
-        <CreateTeam ref={teamRef} />
+        <CreateTeam ref={teamRef} closeModal={closeModal} />
       </dialog>        
     </div>
   );
