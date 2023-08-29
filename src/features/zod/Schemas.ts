665ccb4880b50d05f29658bfbe15c7ce37120dab
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-29 10:55:47 +0300

    Update Schemas.ts
    Added player schema and type

--- src/features/zod/Schemas.ts
+++ src/features/zod/Schemas.ts
@@ -29,4 +29,27 @@ export const ManagerSchema = z.object({
 
 export type ManagerSchemaType = z.infer<typeof ManagerSchema>
 
+export const PlayerSchema = z.object({
+    firstName: z.string().min(2, "First name must be at least 2 characters long"),
+    lastName: z.string().min(2, "Last name must be at least 2 characters long"),
+    age: z.number().positive("Age cannot be negative").min(18,"Manager age must be greater than 18"),
+    shirtNumber: z.number().positive("Shirt number cannot be negative"),
+    position: z.enum([
+      "Center Forward",
+      "Defensive Midfielder",
+      "Attacking Midfielder",
+      "Goal Keeper",
+      "Left Back",
+      "Right Back",
+      "Left Winger",
+      "Right Winger",
+      "Left Center Back",
+      "Right Center Back",
+    ]),
+    foot: z.enum(["Left", "Right", "Both"]),    
+    team: z.number("Enter a valid team")
+})
+
+export type PlayerSchemaType = z.infer<typeof PlayerSchema>
+
 
