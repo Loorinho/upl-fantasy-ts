d22ff091cd04e64f469b1175ab108654bfec092f
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-29 09:46:34 +0300

    Update Schemas.ts
    Added user schema and login schema

---
+++ src/features/zod/Schemas.ts
@@ -0,0 +1,21 @@
+import {z} from 'zod';
+
+export const UserSchema = z.object({
+    firstName: z.string().min(2, "First name must be at least 2 characters long"),
+    lastName: z.string().min(2, "Last name must be at least 2 characters long"),
+    email: z.string().email("Please enter a valid email address"),
+    password: z.string().min(6),
+    confirmPassword: z.string().min(6),
+
+}).refine(data => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+})
+
+export const LoginSchema = z.object({
+    email: z.string().email("Please enter a valid email address"),
+    password: z.string().min(6),
+})
+
+export type LoginSchemaType = z.infer<typeof LoginSchema>
+
