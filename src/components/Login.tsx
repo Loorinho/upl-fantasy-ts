18ed4a9910de4c7bd6b37282539f181e856e5b6a
Jacobson <romeojacobson@gmail.com>
2023-08-29 10:43:18 +0300

    fixed login errors and styles

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,35 +1,78 @@
-import {useForm} from 'react-hook-form';
-import { LoginSchema, LoginSchemaType } from '../features/zod/Schemas';
-import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from "react-hook-form";
+import { LoginSchema, LoginSchemaType } from "../features/zod/Schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-
-  const {register, handleSubmit, formState: {errors, isSubmitting}} = useForm<LoginSchemaType>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginSchemaType>({
     resolver: zodResolver(LoginSchema),
   });
 
   const submitForm = (data: LoginSchemaType) => {
     console.log(data);
-
-  }
+  };
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex justify-center items-center  absolute inset-1">
       {/* <div className="w-50"> */}
-        <h1 className="text-3xl font-bold text-center">Login</h1>
-        <form onSubmit={handleSubmit(submitForm)}>
-          <div className="flex flex-col space-y-2">
-            <div>
-          <label htmlFor="" className='block text-gray-500 text-sm'>Username</label>
-            <input type="text"  className="border-2 border-gray-400 rounded-md p-2" {...register("email")} />
-            {errors.email && <p className="text-red-500">{`${errors.email.message}`}</p>}
-            </div>
-            <input type="password" placeholder="Password" className="border-2 border-gray-400 rounded-md p-2" {...register("password")} />
-            {errors.password && <p className="text-red-500">{`${errors.password.message}`}</p>}
-            <button type="submit" disabled={isSubmitting} className="bg-blue-700 text-white px-4 py-2 m-2 outline-none rounded">
-              Login
-            </button>
+      <form
+        onSubmit={handleSubmit(submitForm)}
+        className="border-2 border-blue-600 py-7 px-5 rounded-md login_form"
+      >
+        <div className="my-10 flex flex-col items-center gap-3">
+          <p className="text-3xl text-blue-700">UPL Fantasy</p>
+          <p className="text-xl font-bold text-center text-blue-600 ">
+            Login to your account
+          </p>
+        </div>
+        <div className="">
+          <div>
+            <label htmlFor="" className="block text-gray-500 text-sm">
+              Username
+            </label>
+            <input
+              type="text"
+              className="border-2  rounded-md p-2 outline-none focus:ring-1 focus:ring-blue-700 w-full"
+              {...register("email")}
+            />
+            {errors.email && (
+              <p className="text-red-500 mb-2">{`${errors.email.message}`}</p>
+            )}
           </div>
-        </form>
+          <div>
+            <label htmlFor="" className="block text-gray-500 text-sm mt-2">
+              Password
+            </label>
+
+            <input
+              type="password"
+              placeholder="Password"
+              className="border-2  rounded-md p-2 outline-none focus:ring-1 focus:ring-blue-700 w-full"
+              {...register("password")}
+            />
+            {errors.password && (
+              <p className="text-red-500">{`${errors.password.message}`}</p>
+            )}
+          </div>
+          <button
+            type="submit"
+            disabled={isSubmitting}
+            className="bg-blue-700 text-white px-4 py-2 mt-2 outline-none rounded w-full"
+          >
+            Login
+          </button>
+
+          <p className="mt-2">
+            Don't have an account?{" "}
+            <Link to={"/register"}>
+              <span className=" text-blue-600 ml-2 mr-1">Register</span>here
+            </Link>
+          </p>
+        </div>
+      </form>
       {/* </div> */}
     </div>
   );
--- src/features/zod/Schemas.ts
+++ src/features/zod/Schemas.ts
@@ -14,7 +14,7 @@ export const UserSchema = z.object({
 
 export const LoginSchema = z.object({
     email: z.string().email("Please enter a valid email address"),
-    password: z.string().min(6),
+    password: z.string().min(8,"Enter your password"),
 })
 
 export type LoginSchemaType = z.infer<typeof LoginSchema>
