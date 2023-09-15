8cb4cbfd72be0b8e840c91e43061a104b7d03ade
Jacobson <romeojacobson@gmail.com>
2023-09-15 16:04:25 +0300

    fixed type safety error

--- src/main.tsx
+++ src/main.tsx
@@ -7,17 +7,28 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ToastContainer } from "react-toastify";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+import store from "./store/store.ts";
+import { Provider } from "react-redux";
+
+let persistor = persistStore(store);
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <App />
-        <ToastContainer />
-        <ReactQueryDevtools />
-      </QueryClientProvider>
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+          <PersistGate persistor={persistor}>
+            <App />
+          </PersistGate>
+          <ToastContainer />
+          <ReactQueryDevtools />
+        </QueryClientProvider>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
