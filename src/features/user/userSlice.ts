19fd6d4ce93d24e9a48839af20967562c4bd98ab
Jacobson <romeojacobson@gmail.com>
2023-09-15 16:48:20 +0300

    corrected user slice error

--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -1,14 +1,14 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
  type UserType = {
     firstName: string,
     lastName: string,
     email: string
  }
- type TInitialState = {
+ type InitialState = {
     myuser: UserType
  }
-const initialState: TInitialState = {
+const initialState: InitialState = {
     myuser: {
         firstName: "Loor",
         lastName: "Inho",
@@ -19,6 +19,13 @@ const userSlice = createSlice({
     name: "user",
     initialState,
     reducers: {
+        setUser: (state, action:PayloadAction<UserType> ) =>{
+            const data = action.payload
+            state.myuser = data
+        },
 
     },
 })
+
+export const { setUser } = userSlice.actions
+export default userSlice.reducer
