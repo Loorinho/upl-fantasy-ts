6aa8e5acf9afe05d18262d298399624febbef915
Jacobson <romeojacobson@gmail.com>
2023-09-15 16:00:33 +0300

    added type safety to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,1 +1,34 @@
-// import { createStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import PlayerReducer from "../features/players/playerSlice";
+
+import storage from "redux-persist/lib/storage";
+import {persistReducer, FLUSH, PERSIST, PAUSE, REGISTER, REHYDRATE, PURGE} from 'redux-persist'
+
+
+
+const persistConfiguration = {
+    key: "root",
+    version: 1,
+    storage
+}
+
+const reducer = combineReducers({
+    player: PlayerReducer
+})
+
+const persistedReducer = persistReducer(persistConfiguration, reducer)
+
+export const store = configureStore({
+    reducer: persistedReducer,
+     middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+export default store
