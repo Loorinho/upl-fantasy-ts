c0ccb5f0718b53b35136cef8849ba52b0e22323e
Jacobson <romeojacobson@gmail.com>
2023-09-15 15:57:03 +0300

    added player slice

---
+++ src/features/players/playerSlice.ts
@@ -0,0 +1,41 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit"
+
+
+type PlayerType = {
+    id: number,
+    firstName: string,
+    lastName: string,
+    position: string,
+    foot: string,
+    team: number
+}
+
+type InitialState = {
+    player: PlayerType,
+    players: PlayerType[]
+}
+
+const initialState: InitialState = {
+    player: {} as PlayerType,
+    players: []
+
+}
+export const playerSlice = createSlice({
+    name: "player",
+    initialState,
+    reducers: {
+        setPlayers: (state, action:PayloadAction<PlayerType[]> ) =>{
+            const data = action.payload
+            state.players = data
+        },
+        setPlayer: (state, action:PayloadAction<PlayerType> ) =>{
+            const data = action.payload
+            state.player = data
+        },
+       
+    }
+})
+
+
+export const { setPlayers, setPlayer } = playerSlice.actions
+export default playerSlice.reducer
